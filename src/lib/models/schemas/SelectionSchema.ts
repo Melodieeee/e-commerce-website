898952fd5f2866b6bfe9fac3f4ccc4b7283981c4
Nien-Chi Melody Yu <m4745melody0706@gmail.com>
2024-12-04 01:31:58 -0800

    vercel deploy 6th try

--- src/lib/models/schemas/SelectionSchema.ts
+++ src/lib/models/schemas/SelectionSchema.ts
@@ -5,7 +5,7 @@ const SelectionSchema = new Schema<ISelection>({
   selectionName: { type: String, required: true },
   price: { type: Number, required: true },
   hasExplainPic: { type: Boolean, required: true },
-  explainPic: { type: String },
+  explainPic: { type: String, required: false },
   customChoice: { type: String, required: false }
 });
 
